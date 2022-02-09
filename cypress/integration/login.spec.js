1a19f6784e43343a9d6431c60577c18bb35683af
leslitaS <leslita.scg@gmail.com>
2022-02-08 22:47:47 -0300

    meu primeiro commit

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,34 @@
+///<reference types="cypress" />
+
+context('Funcionalidade Login', () =>{
+
+    it('Deve fazer login com sucesso' , () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, aluno_ebac')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inseir usuario invalido', () =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('alun_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido.')
+
+    })
+   
+    it('Deve exibir uma mensagem de erro ao inseir senha invalida', () =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain','A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+
+    })
+})
