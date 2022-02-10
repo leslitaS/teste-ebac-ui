e7060a84ffee667c5fb02751d220714607cb8f46
leslitaS <leslita.scg@gmail.com>
2022-02-09 21:00:31 -0300

    alteracoes no produtos e pre-cadastro

--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -16,13 +16,20 @@ describe('Funcionalidade Pagina de produtos', () => {
     });
 
     it('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 3
         cy.get('[class="product-block grid"]')
             .contains('Ariel Roll Sleeve Sweatshirt').click()
         
         cy.get('.button-variable-item-M').click()
-        cy.get('.button-variable-item-Purple').click()
-        cy.get('.input-text').clear().type('2')
+        cy.get('.button-variable-item-S').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.input-text').clear().type(quantidade)
         cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain' ,quantidade)
+
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+        
     });
     
 });
