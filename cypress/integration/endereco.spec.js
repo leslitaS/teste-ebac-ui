8c93713722ea5192e351798a54aa0768d68c06e2
leslitaS <leslita.scg@gmail.com>
2022-02-11 19:30:58 -0300

    page objects

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,5 +1,11 @@
 /// <reference types="cypress" />
 
+import enderecoPage from '../support/page-objects/endereco.page';
+//import EnderecoPage from '../support/page-objects/endereco.page'
+
+const dadosEndereco = require('../fixtures/endereco.json')
+
+
 describe('Funcionalidade Endereços - Faturamento e Entrega', ()=>{
 
     beforeEach(() => {
@@ -11,8 +17,27 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', ()=>{
 
     it('Deve fazer cadastro de faturamento com sucesso', ()=>{
       
-        // cadastro
+        enderecoPage.editarEnderecoFaturamento('skarley','gamarra','google','Brasil', 'Av Brasil','3022','São Paulo','São Paulo','02562307','1199999099','testando@teste.com')
+        cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
 
     });
 
+    it('Deve fazer cadastro de faturamento com sucesso -  Usando Arquivo de Dados', ()=>{
+      
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
+
+    });
 });
