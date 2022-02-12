cdd49a8d54aae891d3e4baee6ae3b77c711213ef
leslitaS <leslita.scg@gmail.com>
2022-02-11 21:55:15 -0300

    editando endereço entrega

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -4,6 +4,7 @@ import enderecoPage from '../support/page-objects/endereco.page';
 //import EnderecoPage from '../support/page-objects/endereco.page'
 
 const dadosEndereco = require('../fixtures/endereco.json')
+const dadosEnderecoEntrega = require('../fixtures/enderecoEntrega.json')
 
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', ()=>{
@@ -39,5 +40,27 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', ()=>{
             )
         cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
 
+    });
+
+    it('Deve fazer cadastro de Endereço de entrega com sucesso', ()=>{
+      
+        enderecoPage.editarEnderecoEntrega('skarley','gamarra','Google','Brasil', 'Av Brasil','3022','São Paulo','São Paulo','02562307')
+        cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
+
+    });
+    it.only('Deve fazer cadastro de Endereço de entrega com sucesso - Usando Arquivo de Dados', () => {
+        enderecoPage.editarEnderecoEntrega(
+            dadosEnderecoEntrega[1].nome,
+            dadosEnderecoEntrega[1].sobrenome,
+            dadosEnderecoEntrega[1].empresa,
+            dadosEnderecoEntrega[1].pais,
+            dadosEnderecoEntrega[1].endereco,
+            dadosEnderecoEntrega[1].complemento,
+            dadosEnderecoEntrega[1].cidade,
+            dadosEnderecoEntrega[1].estado,
+            dadosEnderecoEntrega[1].cep
+            )
+        cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
+        
     });
 });
